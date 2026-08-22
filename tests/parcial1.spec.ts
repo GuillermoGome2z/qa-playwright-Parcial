b5211b225b7ed2dea7ca82fe5d4457ab6680024f
Guillermo Jose Gomez Aguilera <gomezguille391@gmail.com>
2026-08-22 10:52:53 -0600

    Test numero 3

--- tests/parcial1.spec.ts
+++ tests/parcial1.spec.ts
@@ -13,9 +13,21 @@ test('verificar inicio de sesion', async ({ page }) => {
     await page.fill('#password', 'SuperSecretPassword!');
     await page.click('button[type="submit"]');
 
-    await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/secure');
+    await expect(page).toHaveURL('https://practice.expandtesting.com/login/secure');
     await expect(page.locator('h1')).toHaveText('Logged In Successfully');
 });
+
+test('login con credenciales invalidas', async ({ page }) => {
+    await page.goto('');
+
+    await page.fill('#username', 'guille');
+    await page.fill('#password', '12345!');
+    await page.click('button[type="submit"]');
+
+    await expect(page).toHaveURL('https://practice.expandtesting.com/login/secure');
+    await expect(page.locator('h1')).toHaveText('Logged In Successfully');
+}); 
+
 
 
 
