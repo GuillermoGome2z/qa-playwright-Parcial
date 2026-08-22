10a19e526b5fb87bb42fb2430d53ff29e7de2ce9
Guillermo Jose Gomez Aguilera <gomezguille391@gmail.com>
2026-08-22 10:35:35 -0600

    Segundo test

--- tests/parcial1.spec.ts
+++ tests/parcial1.spec.ts
@@ -6,6 +6,19 @@ test('verifica el titulo de la pagina de login', async ({ page }) => {
 	await expect(page).toHaveTitle('Test Login Page for Automation Testing Practice');
 });
 
+test('verificar inicio de sesion', async ({ page }) => {
+    await page.goto('');
+
+    await page.fill('#username', 'practice');
+    await page.fill('#password', 'SuperSecretPassword!');
+    await page.click('button[type="submit"]');
+
+    await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+    await expect(page.locator('h1')).toHaveText('Logged In Successfully');
+});
+
+
+
 
 
 
