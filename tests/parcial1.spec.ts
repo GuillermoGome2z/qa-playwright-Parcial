39937a6e59e4c65cf60ee81a9cd0caf8c778b004
Guillermo Jose Gomez Aguilera <gomezguille391@gmail.com>
2026-08-22 10:40:58 -0600

    Agrega prueba de titulo y evidencias

--- tests/parcial1.spec.ts
+++ tests/parcial1.spec.ts
@@ -13,9 +13,11 @@ test('verificar inicio de sesion', async ({ page }) => {
     await page.fill('#password', 'SuperSecretPassword!');
     await page.click('button[type="submit"]');
 
-    await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+    await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/secure');
     await expect(page.locator('h1')).toHaveText('Logged In Successfully');
 });
+
+
 
 
 
